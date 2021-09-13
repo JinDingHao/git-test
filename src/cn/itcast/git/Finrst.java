dcb801de5f0f9f6edd761ab68ad74e7de4188abb
JinDinHao <2969432675@qq.com>
2021-09-13 10:57:44 +0800

    提交代码

---
+++ src/cn/itcast/git/Finrst.java
@@ -0,0 +1,21 @@
+package cn.itcast.git;
+
+/**
+ * @author: 苦逼程序员
+ **/
+public class Finrst {
+    public static void main(String[] args) {
+        System.out.println("hellow project");
+        System.out.println("hellow project");
+        System.out.println("hellow project");
+        System.out.println("hellow project");
+        System.out.println("hellow project");
+        System.out.println("hellow project");
+        Finrst finrst = new Finrst();
+        finrst.test();
+
+    }
+    public  void  test(){
+        System.out.println("hellow git project");
+    }
+}
